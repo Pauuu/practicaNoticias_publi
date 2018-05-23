e4b9ff6f4aba88074877334664c9faa347fde4ba
Unknown <belcegorx@gmail.com>
2018-05-23 12:54:26 +0200

    En este repositorio o en el original?

---
+++ resources/publi/jspubli/jspubli.js
@@ -0,0 +1,22 @@
+var numImagen = 1;
+var contador = 0;
+var imagenes = [];
+
+$(document).ready(function () {
+    imagenes[0] = "img/imgp1.jpg"
+    imagenes[1] = "img/imgp2.jpg"
+    imagenes[2] = "img/imgp2.jpg"
+
+    var timer = setTimeout(cambiarPubli, 3000);
+
+});
+
+function cambiarPubli() {
+    if (contador > imagenes.length){
+        contador = 0;
+    } else {
+        contador ++;
+    }
+
+    $("#imgPubli").attr("src", imagenes[contador]);
+}
